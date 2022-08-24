aaaaca4e139f37eb39970bd54a617d11b42165fe
dumengru <dmr123.com>
2022-08-24 10:36:16 +0800

    首次添加

---
+++ poker.py
@@ -0,0 +1,18 @@
+
+
+class Poker:
+    suits = ("hearts", "clubs", "diamonds", "spades")
+
+    def __init__(self):
+        self.poker = 0
+
+    pass
+
+
+class Person:
+    pass
+
+
+class Game:
+    pass
+
